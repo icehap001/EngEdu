2c40037d93d9e39848f6bc5804239f510231e214
icehap001 <icehap001@gmail.com>
2017-10-19 23:28:48 +0700

    asd

--- Edu/Edu/Program.cs
+++ Edu/Edu/Program.cs
@@ -10,7 +10,11 @@ namespace Edu
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Year 2558 semester 1,Test .");
+            Console.WriteLine("Year 2558 semester 2,Test .");
+            Console.WriteLine("Year 2559 semester 1,Test .");
+            Console.WriteLine("Year 2559 semester 2,Test .");
+           
         }
     }
 }

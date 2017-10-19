616633ee35faf0e02c7dd0265364aee40593120c
icehap001 <icehap001@gmail.com>
2017-10-19 17:49:49 +0700

    first year semester 1

---
+++ Edu/Edu/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Year 2560 semester 1, I am an Engedu freshman.");
+        }
+    }
+}
